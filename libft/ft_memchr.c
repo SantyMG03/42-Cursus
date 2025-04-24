86375490e01dc5f403509f018d5f66d6e555faea
SantyMG03 <santymorenog2003@gmail.com>
2025-04-24 20:19:27 +0200

    Modificado makefile

---
+++ libft/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: santmore <santmore@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 20:09:58 by santmore          #+#    #+#             */
+/*   Updated: 2025/04/24 20:13:18 by santmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((void *)((unsigned char *)s + i));
+		i++;
+	}
+	return (NULL);
+}
