8bfdfc908af3a26b94c28c3e4201852a945eb93d
SantyMG03 <santymorenog2003@gmail.com>
2025-04-25 11:30:15 +0200

    Agregado memcmp

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: santmore <santmore@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 11:19:15 by santmore          #+#    #+#             */
+/*   Updated: 2025/04/25 11:24:39 by santmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = s1;
+	str2 = s2;
+	i = 0;
+	while (i < n && str1[i] == str2[i])
+	{
+		i++;
+	}
+	if (i == n)
+		return (0);
+	return (str1[i] - str2[i]);
+}
