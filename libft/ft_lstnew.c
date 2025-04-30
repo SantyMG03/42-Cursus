2dfcd5f417dbb1acb1dfcd9d70435f982dba716d
SantyMG03 <santymorenog2003@gmail.com>
2025-04-30 18:40:44 +0200

    Comienzo de la parte bonus

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: santmore <santmore@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 18:34:37 by santmore          #+#    #+#             */
+/*   Updated: 2025/04/30 18:39:28 by santmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list *node = malloc(sizeof(t_list));
-	if(!node)
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
 		return (NULL);
 	node->content = content;
 	node->next = NULL;
