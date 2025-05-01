a5af36e636c1383c324c08adee462378b7b172c6
SantyMG03 <santymorenog2003@gmail.com>
2025-05-02 01:57:15 +0200

    Retoques

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -15,12 +15,6 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (!s)
-        return;
-    while (*s)
-    {
-        write(fd, s, 1);
-        s++;
-    }
+    ft_putstr_fd(s, fd);
     write(fd, "\n", 1);
 }
