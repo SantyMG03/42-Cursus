31472f22feb7f392ca70c7e65b9e96b6b1d7da14
SantyMG03 <santymorenog2003@gmail.com>
2025-05-15 23:02:13 +0200

    Inicio get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,9 +6,41 @@
 /*   By: santmore <santmore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 21:57:31 by santmore          #+#    #+#             */
-/*   Updated: 2025/05/15 22:26:22 by santmore         ###   ########.fr       */
+/*   Updated: 2025/05/15 22:59:29 by santmore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// join + free
+char	*ft_free(char *str1, char *str2)
+{
+	char	*temp;
+
+	temp = ft_strjoin(str1, str2);
+	free(str1);
+	return (temp);
+}
+
+char	*ft_next(char *buf)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	while (buf[i] && buf[i] != '\n')
+		i++;
+	if (!buf[i])
+	{
+		free(buf);
+		return (NULL);
+	}
+	line = ft_calloc((ft_strlen(buf) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buf[i])
+		line[j++] = buf[i++];
+	free(buf);
+	return (line);
+}
