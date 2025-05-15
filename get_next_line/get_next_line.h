d2593e0c8436bcbe243f88f37c798935477b6cb8
SantyMG03 <santymorenog2003@gmail.com>
2025-05-15 22:19:52 +0200

    Inicio proyecto

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: santmore <santmore@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 21:57:37 by santmore          #+#    #+#             */
+/*   Updated: 2025/05/15 22:18:39 by santmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int searchedchar);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elcount, size_t elsize);
+
+size_t	ft_strlen(const char *str);
+#endif
