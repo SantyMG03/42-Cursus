ebfd863ba9d230244cdad9352cc77517cc5f80b9
SantyMG03 <santymorenog2003@gmail.com>
2025-06-09 11:58:33 +0200

    Make file + .h

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: santmore <santmore@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 11:43:54 by santmore          #+#    #+#             */
+/*   Updated: 2025/06/09 11:49:01 by santmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_print_char(int character);
+int	ft_printf(const char *str, ...);
+int	ft_print_string(char *str);
+int	ft_print_num(int n);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_hexa(unsigned int n, const char w);
+int	ft_print_pointer(unsigned long long ptr);
+
+#endif
