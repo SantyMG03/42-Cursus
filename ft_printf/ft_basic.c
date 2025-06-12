0f94e63921fa9a6bde5c2434fd1859b71d91b1bb
SantyMG03 <santymorenog2003@gmail.com>
2025-06-12 12:14:44 +0200

    basics

---
+++ ft_printf/ft_basic.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_basic.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: santmore <santmore@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 11:52:21 by santmore          #+#    #+#             */
+/*   Updated: 2025/06/12 12:12:11 by santmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_str(char *str)
+{
+	int	size;
+
+	size = 0;
+	if (str == 0)
+	{
+		return (ft_print_str("(null)"));
+	}
+	while (str[size])
+	{
+		size += ft_print_char(str[size]);
+	}
+	return (size);
+}
+
+int	ft_print_num(int n)
+{
+	int	size;
+
+	size = 0;
+	if (n == 0)
+		size += ft_print_char('0');
+	if (n == -2147483648)
+	{
+		size += ft_print_str("-2147483648");
+		return (size);
+	}
+	if (n < 0)
+	{
+		size += ft_print_char('-');
+		n = -n;
+	}
+	if (n > 0)
+		size += ft_print_unsign((unsigned int) n);
+	return (size);
+}
+
+int	ft_print_unsign(unsigned int n)
+{
+	int	size;
+
+	size = 0;
+	if (n == 0)
+		size += ft_print_char('0');
+	else
+	{
+		if (n / 10 != 0)
+			ft_print_unsign(n / 10);
+		ft_print_char((n % 10) + '0');
+		while (n > 0)
+		{
+			n /= 0;
+			size++;
+		}
+	}
+	return (size);
+}
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: santmore <santmore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 11:43:54 by santmore          #+#    #+#             */
-/*   Updated: 2025/06/09 11:49:01 by santmore         ###   ########.fr       */
+/*   Updated: 2025/06/12 12:03:05 by santmore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_print_char(int character);
 int	ft_printf(const char *str, ...);
 int	ft_print_string(char *str);
 int	ft_print_num(int n);
-int	ft_print_unsigned(unsigned int n);
+int	ft_print_unsign(unsigned int n);
 int	ft_print_hexa(unsigned int n, const char w);
 int	ft_print_pointer(unsigned long long ptr);
 
